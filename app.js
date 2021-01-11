69d6ce392f17727a957a8208b2e71692a4057080
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-01-11 15:07:39 -0800

    add html content

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // import functions and grab DOM elements
-
+const quizButton = document.getElementById("quiz-button");
+const hiddenDiv = document.getElementById("hidden-div");
 // initialize state
 
 // set event listeners to update state and DOM
+
+quizButton.addEventListener('click', () => {
+    alert('Hello! What is your first name?');
+});
