fef95d87396bc2cc5a308e3ff3612df5b4027558
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-01-11 18:50:56 -0800

    fix eslint issues

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 
-import { countsAsAYes } from './counts-as-a-yes.js'
+import { countsAsAYes } from './counts-as-a-yes.js';
 
-const quizButton = document.getElementById("quiz-button");
-const finalScore = document.getElementById("final-score");
+const quizButton = document.getElementById('quiz-button');
+const finalScore = document.getElementById('final-score');
 
 
 quizButton.addEventListener('click', () => {
-    alert("Welcome to the Ocicat quiz!");
+    alert('Welcome to the Ocicat quiz!');
 
-    const confirmation = confirm("Do you want to proceed?");
+    const confirmation = confirm('Do you want to proceed?');
 
     if (!confirmation) return;
 
     let rightAnswers = 0;
 
-    const firstName = prompt("What is your first name?");
-    const lastName = prompt("What is your last name?");
+    const firstName = prompt('What is your first name?');
+    const lastName = prompt('What is your last name?');
 
-    const questionOne = prompt("Do Ocicats have spots");
+    const questionOne = prompt('Do Ocicats have spots');
 
     if (countsAsAYes(questionOne)) rightAnswers++;
 
-    const questionTwo = prompt("Do Ocicats get along with other cats");
+    const questionTwo = prompt('Do Ocicats get along with other cats');
 
     if (countsAsAYes(questionTwo)) rightAnswers++;
 
-    const questionThree = prompt("Can Ocicats fly?");
+    const questionThree = prompt('Can Ocicats fly?');
 
     if (!countsAsAYes(questionThree)) rightAnswers++;
 
