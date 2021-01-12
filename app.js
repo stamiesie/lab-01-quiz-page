82c591d594048e7131f1f6b98d6d849de67014cb
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-01-11 16:13:26 -0800

    quiz prompt and user name

--- app.js
+++ app.js
@@ -6,5 +6,21 @@ const hiddenDiv = document.getElementById("hidden-div");
 // set event listeners to update state and DOM
 
 quizButton.addEventListener('click', () => {
-    alert('Hello! What is your first name?');
+    alert("Welcome to the Ocicat quiz!");
+
+    const confirmation = confirm("Do you want to proceed?");
+
+    if (!confirmation) return;
+
+    let rightAnswers = 0;
+
+    const firstName = prompt("What is your first name?");
+    const lastName = prompt("What is your last name?");
+
+
+    const questionOne = prompt("Do Ocicats have spots");
+
+
+
+
 });
